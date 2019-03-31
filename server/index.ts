a22d2c5aa0e595a269751103d8961bb0331e5621
Aleksander <alek.paliszewski@gmail.com>
2019-03-31 23:36:34 +0200

    + przeniesienie otodom na ts

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,6 @@
 import { OlxScraper } from './scrapers/OlxScraper';
-
+import {OtodomScraper} from "./scrapers/OtodomScraper";
+const otodomScraper: OtodomScraper = new OtodomScraper();
+otodomScraper.process();
 const olxScraper: OlxScraper = new OlxScraper();
 olxScraper.process();
