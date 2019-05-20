1e4488e82bad6ce6f285c21e482bcab76cad4d7b
Konrad Kalemba <konrad_kal@hotmail.com>
2019-05-20 08:45:47 +0200

    wyświetlanie markerów ogłoszeń na mapie

--- frontend/src/components/MapView/index.js
+++ frontend/src/components/MapView/index.js
@@ -2,13 +2,39 @@ import React, { Component } from 'react'
 import L from 'leaflet';
 import styles from './style.module.scss';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import axios from 'axios';
+import Ad from '../Ad'
 
-export default class SimpleExample extends Component{
-    state = {
-        lat: 51.109,
-        lng: 17.034,
-        zoom: 14,
-    };
+export default class SimpleExample extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ads: [],
+            lat: 51.109,
+            lng: 17.034,
+            zoom: 14,
+        }
+    }
+
+
+    componentDidMount() {
+        axios.get('http://localhost:3000')
+            .then(
+                (result) => {
+                    console.log(result.data);
+                    this.setState({
+                        isLoaded: true,
+                        ads: result.data
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
 
     render() {
         const position = [this.state.lat, this.state.lng];
@@ -29,6 +55,13 @@ export default class SimpleExample extends Component{
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
+                    {this.state.ads.filter(ad => ad.street).map(ad => {
+                        return (
+                            <Marker position={[ad.street.lat, ad.street.long]}>
+                                <Popup className={styles.adPopup}><Ad ad={ad} /></Popup>
+                            </Marker>
+                        )
+                    })}
                 </Map>
             </div>
         )
