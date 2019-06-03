10a3db865b0278ee43bfd4fa5bd88d1502c76da1
Konrad Kalemba <konrad_kal@hotmail.com>
2019-06-03 08:34:39 +0200

    customowe markery, grupowanie

--- frontend/src/components/MapView/index.js
+++ frontend/src/components/MapView/index.js
@@ -4,10 +4,23 @@ import styles from './style.module.scss';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 import Ad from '../Ad';
 import { connect } from 'react-redux';
+import ReactDOMServer from 'react-dom/server';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 const mapStateToProps = state => ({
     ads: state.ads
 });
+
+class MarkerIcon extends Component {
+  render() {
+    const price = this.props.price || 'Nieznane';
+    return (
+        <div>
+            {price}
+        </div>
+    );
+  }
+}
 
 class MapView extends Component {
     constructor(props) {
@@ -23,29 +36,46 @@ class MapView extends Component {
     render() {
         const position = [this.state.lat, this.state.lng];
 
+        const createClusterCustomIcon = function (cluster) {
+            return L.divIcon({
+                html: `<div>${cluster.getChildCount()}</div>`,
+                className: styles.clusterMarkerIcon,
+                iconSize: L.point(40, 40, true),
+            });
+        }
+
         return (
             <div className={styles.map}>
                 <Map
                     center={position}
-                    zoom={this.state.zoom}>
+                    zoom={this.state.zoom}
+                    maxZoom={18}
                     attributionControl={true}
                     zoomControl={true}
                     doubleClickZoom={true}
                     scrollWheelZoom={true}
                     dragging={true}
                     animate={true}
-                    easeLinearity={0.35}
+                    easeLinearity={0.35}>
                     <TileLayer
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    {this.props.ads.filter(ad => ad.street).map(ad => {
-                        return (
-                            <Marker position={[ad.street.lat, ad.street.long]}>
-                                <Popup className={styles.adPopup}><Ad ad={ad} /></Popup>
-                            </Marker>
-                        )
-                    })}
+                    <MarkerClusterGroup iconCreateFunction={createClusterCustomIcon}>
+                        {this.props.ads.filter(ad => ad.street).map(ad => {
+                            return (
+                                <Marker
+                                    position={[ad.street.lat, ad.street.long]}
+                                    icon={L.divIcon({
+                                        className: styles.markerIcon,
+                                        html: ReactDOMServer.renderToString(<MarkerIcon price={ad.price}/>)
+                                    })}
+                                >
+                                    <Popup className={styles.adPopup}><Ad ad={ad} /></Popup>
+                                </Marker>
+                            )
+                        })}
+                    </MarkerClusterGroup>
                 </Map>
             </div>
         )
